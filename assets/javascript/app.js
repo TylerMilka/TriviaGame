c0ee07efa91a7895cf09b2dcf3bb71e20a6864f1
Tyler <tgmilka@gmail.com>
2016-11-01 17:32:08 -0500

    asdfads

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ var clock = {
 		//get current time and pass into clock.timeConverter
 		// save the result into a variable.
 		var converted = clock.timeConverter(clock.time);//60
-		console.log(converted);
+		
 		//use the variable created to show the converted time
 		$('#display').html(converted);
 	},
@@ -44,4 +44,39 @@ window.onload = function(){
 /////////////////////////////////////////////////////////////////////////////////////////
 
 //trivia object 
-var
+//functiom 
+var trivia = { 
+
+quizQuestions: function (question, choices, correctAnswer){
+		//THIS refers to the parameters that are being used
+		this.question =	question,
+		this.choices = choices, 
+		this.correctAnswer = correctAnswer,
+	
+	//array that calls on each new question 
+	[
+	new quizQuestion('what is the name of this city', ['Chicago', 'Utah', 'Detroit', 'Phoenix'], 0),
+	new quizQuestion('what is the name of this city', ['Chicago', 'Utah', 'Detroit', 'Phoenix'], 0),
+	new quizQuestion('what is the name of this city', ['Chicago', 'Utah', 'Detroit', 'Phoenix'], 0),
+	];
+
+	$('#question').html(parseInt(
+	console.log("adsfasdfds"); 
+}
+},
+
+
+window.onload = function(){
+	$('#start').on('click',  
+
+	}
+
+	/*
+	var options = quizQuestions.choices;
+	var formHtml = ' ';
+		for (var i = 0; i < options.length; i++){
+			formHtml += '<div><input type = "radio" name = "option" value="' + i + '" id="option' + i + '">
+			<label for = "option' + i + '">' + quizQuestions.choices[i] + '</label></div><br/>';
+		}
+ 	*/
+
